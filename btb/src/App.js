f5486a560a68784a788dfcc1b17351da6a75b746
alvinhu182 <alvinhu@hotmail.com>
2022-03-29 19:56:27 -0300

    corrigir os erros ainda

--- btb/src/App.js
+++ btb/src/App.js
@@ -1,11 +1,60 @@
-import {Button}from 'react-bootstrap'
-function App() {
+import {Button, Container, Nav, Navbar}from 'react-bootstrap'
+import Logo from './assets/img/logo-ggeasy.png'
+import Banner from './assets/img/Baner-ggeasy.jpg'
+
+function Header (){
   return (
-    <div>
-    <button className="btn btn-primary">Teste</button>
-    <Button>Teste</Button>
-    </div>
-  );
+    <header>
+     
+      <Navbar expand='md'
+      className='main-navbar'>
+        <Container>
+          <Navbar.Brand href='/'>
+              <img src={Logo} alt='ggeasy width={181} height={50} />
+            
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls = 'navbar-header' />
+         
+          <Navbar.Collapse id='navbar-header'>
+              <nav className='ms-auto'>
+                  <Nav.Link href='/'> Home </Nav.Link>
+                  <Nav.Link href='/cursos'> Cursos </Nav.Link>
+              </nav>
+          </Navbar.Collapse>
+       </Container>
+      </Navbar>
+    </header>
+  )
 }
+function HomeView (){
+  return (
+  <>
+    <Header/>
+    <main>
+      <div className='shadow border p-4 my-3 banner-home'>
+        <h1>Bem vindo ao gg easy</h1>
+        <p>treta</p>
+        <p> venha aprender com a gente</p>
+        <Button>aulas</Button>
+        <img src={Banner} alt='ggeasy' width ={626} height={391} className='img-fluid'/>
+      </div>
+    <p>Conteudo</p>
+    </main>
+    <Footer>
+  </>
+  )
+}
+function Footer(){
+  return(
+    <footer className='footer text-center text-white py-2'>
+      <p className='m-0 mt-4'>aprenda como é fácil jogar bem sem muita dificuldade</p>
+    </footer>
+  )
+}
+function App(){
+  return <HomeView/>;
+}
+
+
 
 export default App;
